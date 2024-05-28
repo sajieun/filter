dea51b3656d2db36e3b779f18e09a8af9de90f97
sajieun <sajieun000721@naver.com>
2024-05-28 14:45:59 +0900

    getReader() has already been called for this request 오류

--- src/main/java/com/example/filter/filter/LoggerFilter.java
+++ src/main/java/com/example/filter/filter/LoggerFilter.java
@@ -3,7 +3,12 @@ package com.example.filter.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -12,7 +17,17 @@ public class LoggerFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // 진입전
         log.info(">>>>> 진입 ");
-        chain.doFilter(request, response);
+
+        var req = new HttpServletRequestWrapper((HttpServletRequest) request);
+        var res = new HttpServletResponseWrapper((HttpServletResponse) response);
+
+        var br = req.getReader();
+        var list = br.lines().collect(Collectors.toList());
+        list.forEach(it->{
+            log.info("{}",it);
+        });
+
+        chain.doFilter(req, res);
         log.info("<<<<< 리턴");
         // 진입후
 
